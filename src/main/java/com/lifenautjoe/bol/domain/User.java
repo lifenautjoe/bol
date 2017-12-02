4b3d587e5a26643bae78d315d12657de7dac9ffb
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-03 00:12:58 +0100

    feat(User): create

---
+++ src/main/java/com/lifenautjoe/bol/domain/User.java
@@ -0,0 +1,13 @@
+package com.lifenautjoe.bol.domain;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
