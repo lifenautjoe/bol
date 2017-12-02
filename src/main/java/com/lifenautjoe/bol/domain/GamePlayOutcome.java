2e17a6c4c980dcb4decbbdf27007ff4fd542eb2d
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-03 00:12:34 +0100

    feat(GamePlayOutcome): create

---
+++ src/main/java/com/lifenautjoe/bol/domain/GamePlayOutcome.java
@@ -0,0 +1,13 @@
+package com.lifenautjoe.bol.domain;
+
+public class GamePlayOutcome {
+    private boolean userHasAnotherTurn;
+
+    public boolean isUserHasAnotherTurn() {
+        return userHasAnotherTurn;
+    }
+
+    public void setUserHasAnotherTurn(boolean userHasAnotherTurn) {
+        this.userHasAnotherTurn = userHasAnotherTurn;
+    }
+}
