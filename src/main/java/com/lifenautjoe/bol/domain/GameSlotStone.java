bbaed3ffba0323d6edba460c9321aeac051d3d26
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-03 00:12:02 +0100

    feat(GameSlotStone): create

---
+++ src/main/java/com/lifenautjoe/bol/domain/GameSlotStone.java
@@ -0,0 +1,13 @@
+package com.lifenautjoe.bol.domain;
+
+public class GameSlotStone {
+    private final int id;
+
+    public GameSlotStone(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
