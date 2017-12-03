c51652b1b1cb92d6d1723794aa3a2380cfa7d696
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-03 21:51:09 +0100

    feat(GamePlayRequestBody): create

---
+++ src/main/java/com/lifenautjoe/bol/controllers/games/GamePlayRequestBody.java
@@ -0,0 +1,18 @@
+package com.lifenautjoe.bol.controllers.games;
+
+public class GamePlayRequestBody {
+    private int slotId;
+
+
+    public GamePlayRequestBody() {
+
+    }
+
+    public int getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
+}
