6ab2a0295ff03bb9797621cf34953ede5e16dfe6
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-03 21:52:24 +0100

    feat(User): hold reference to curr game

--- src/main/java/com/lifenautjoe/bol/domain/User.java
+++ src/main/java/com/lifenautjoe/bol/domain/User.java
@@ -2,6 +2,7 @@ package com.lifenautjoe.bol.domain;
 
 public class User {
     private String name;
+    private Game currentGame;
 
     public User(String name) {
         this.name = name;
@@ -9,5 +10,21 @@ public class User {
 
     public String getName() {
         return name;
+    }
+
+    public Game getCurrentGame() {
+        return currentGame;
+    }
+
+    public void setCurrentGame(Game currentGame) {
+        this.currentGame = currentGame;
+    }
+
+    public boolean hasGame() {
+        return this.currentGame == null;
+    }
+
+    public boolean isPlayingGame() {
+        return currentGame != null;
     }
 }
