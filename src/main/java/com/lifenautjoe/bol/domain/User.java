3fc3972cad07d29c6cd6730cd52b4d6d510ab4b4
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-04 21:36:42 +0100

    feat(User): override equals and hashCode

--- src/main/java/com/lifenautjoe/bol/domain/User.java
+++ src/main/java/com/lifenautjoe/bol/domain/User.java
@@ -1,11 +1,33 @@
 package com.lifenautjoe.bol.domain;
 
+import java.util.Objects;
+
 public class User {
     private String name;
     private Game currentGame;
 
     public User(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!User.class.isAssignableFrom(obj.getClass())) {
+            return false;
+        }
+        final User other = (User) obj;
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        return true;
     }
 
     public String getName() {
@@ -22,9 +44,5 @@ public class User {
 
     public boolean hasGame() {
         return this.currentGame == null;
-    }
-
-    public boolean isPlayingGame() {
-        return currentGame != null;
     }
 }
