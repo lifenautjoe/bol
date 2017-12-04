a3d19fe1d4093f550bb3bd4b5f17cfe9e0c81c21
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-04 20:11:17 +0100

    feat(UsersController): login and logout

--- src/main/java/com/lifenautjoe/bol/controllers/users/UsersController.java
+++ src/main/java/com/lifenautjoe/bol/controllers/users/UsersController.java
@@ -1,10 +1,13 @@
 package com.lifenautjoe.bol.controllers.users;
 
+import com.lifenautjoe.bol.domain.User;
 import com.lifenautjoe.bol.services.UsersManagerService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping(path = "users")
@@ -12,13 +15,36 @@ public class UsersController {
 
     private UsersManagerService usersManagerService;
 
-    @RequestMapping(path = "login", method = RequestMethod.POST)
-    public void login(@RequestBody LoginRequestBody body) {
+    @Autowired
+    public UsersController(UsersManagerService usersManagerService) {
+        this.usersManagerService = usersManagerService;
+    }
 
+    @RequestMapping(path = "login", method = RequestMethod.POST)
+    public ResponseEntity<?> login(@RequestBody LoginRequestBody body, HttpSession httpSession) {
+
+        String userName = body.getUserName();
+
+        User user = null;
+        if (usersManagerService.sessionHasUser(httpSession)) {
+            user = usersManagerService.getUserFromSession(httpSession);
+        } else {
+            if (usersManagerService.userNameAlreadyExists(userName)) {
+                return ResponseEntity
+                        .status(HttpStatus.CONFLICT)
+                        .body("User name already taken");
+            }
+            user = usersManagerService.createUserForSession(body.getUserName(), httpSession);
+        }
+
+        return ResponseEntity.ok().body(user);
     }
 
     @RequestMapping(path = "logout", method = RequestMethod.POST)
-    public void logout() {
-
+    @ResponseStatus(value = HttpStatus.OK)
+    public void logout(HttpSession httpSession) {
+        if (usersManagerService.sessionHasUser(httpSession)) {
+            usersManagerService.removeUserForSession(httpSession);
+        }
     }
 }
