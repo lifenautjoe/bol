3f63cc641c8c0129c8a56ce0ae95eb4ad4d53255
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-04 20:10:36 +0100

    feat(LoginRequestBody): add getters n setters for username

--- src/main/java/com/lifenautjoe/bol/controllers/users/LoginRequestBody.java
+++ src/main/java/com/lifenautjoe/bol/controllers/users/LoginRequestBody.java
@@ -2,5 +2,16 @@ package com.lifenautjoe.bol.controllers.users;
 
 
 public class LoginRequestBody {
-    private String username;
+    private String userName;
+
+    public LoginRequestBody() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
