4c816b8138de0c4f1d68b7ae5095589cb45bc4f1
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-07 23:23:20 +0100

    feat(GamesController): use ApiResponse class

--- src/main/java/com/lifenautjoe/bol/controllers/games/GamesController.java
+++ src/main/java/com/lifenautjoe/bol/controllers/games/GamesController.java
@@ -1,11 +1,15 @@
 package com.lifenautjoe.bol.controllers.games;
 
+import com.lifenautjoe.bol.controllers.ApiResponse;
+import com.lifenautjoe.bol.controllers.games.requests.GamePlayRequestBody;
+import com.lifenautjoe.bol.controllers.games.responses.GameResponse;
 import com.lifenautjoe.bol.domain.Game;
 import com.lifenautjoe.bol.domain.GamePlayOutcome;
 import com.lifenautjoe.bol.domain.User;
 import com.lifenautjoe.bol.services.games.GamesManagerService;
 import com.lifenautjoe.bol.services.users.UsersManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -13,7 +17,9 @@ import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/games")
@@ -21,28 +27,39 @@ public class GamesController {
     private SimpMessagingTemplate template;
     private GamesManagerService gamesManagerService;
     private UsersManagerService usersManagerService;
+    private ConversionService conversionService;
 
     @Autowired
     public GamesController(SimpMessagingTemplate messagingTemplate,
                            GamesManagerService gamesManagerService,
-                           UsersManagerService usersManagerService) {
+                           UsersManagerService usersManagerService,
+                           ConversionService conversionService) {
         this.template = messagingTemplate;
         this.gamesManagerService = gamesManagerService;
         this.usersManagerService = usersManagerService;
+        this.conversionService = conversionService;
     }
 
 
     @RequestMapping(path = "")
-    public Collection<Game> getAll() {
-        return gamesManagerService.getAll();
+    public ResponseEntity<Object> getAll() {
+        Collection<Game> games = gamesManagerService.getAll();
+        List<GameResponse> responseGames = new ArrayList<>();
+
+        for (Game game : games) {
+            GameResponse gameReponse = conversionService.convert(game, GameResponse.class);
+            responseGames.add(gameReponse);
+        }
+        return ResponseEntity.ok().body(responseGames);
     }
 
     @RequestMapping(path = "/{gameName}/join")
-    public ResponseEntity<?> joinGame(@PathVariable("gameName") String gameName, HttpSession httpSession) {
+    public ResponseEntity<Object> joinGame(@PathVariable("gameName") String gameName, HttpSession httpSession) {
+
         if (!usersManagerService.sessionHasUser(httpSession)) {
             return ResponseEntity
                     .status(HttpStatus.FORBIDDEN)
-                    .body("User is not logged in");
+                    .body(new ApiResponse("User must be logged in before joining a game"));
         }
 
         User user = usersManagerService.getUserFromSession(httpSession);
@@ -50,7 +67,7 @@ public class GamesController {
         if (user.hasGame()) {
             return ResponseEntity
                     .status(HttpStatus.CONFLICT)
-                    .body("User already has game");
+                    .body(new ApiResponse("User already has a game"));
         }
 
         Game game = gamesManagerService.getOrCreateGameWithName(gameName);
@@ -58,12 +75,12 @@ public class GamesController {
         if (game.isFull()) {
             return ResponseEntity
                     .status(HttpStatus.FORBIDDEN)
-                    .body("Game is full");
+                    .body(new ApiResponse("Game is full"));
         }
 
         user.setGame(game);
 
-        return ResponseEntity.ok("Game joined!");
+        return ResponseEntity.ok(new ApiResponse("Game joined!"));
     }
 
     @RequestMapping(path = "/{gameName}/play", method = RequestMethod.POST)
